cebccfff089e226023c621f9b4411f8001001bfd
Yinggang.Dong <dyggod@163.com>
2022-07-22 17:35:38 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  title: 'dyggod\'s blog',
+  description: 'Just playing around',
+  themeConfig: {
+    logo: '/assets/img/logo.png',
+    nav: [
+      { text: 'Guide', link: '/guide' },
+      {
+        text: 'Dropdown Menu',
+        items: [
+          { text: 'Item A', link: '/item-1' },
+          { text: 'Item B', link: '/item-2' },
+          { text: 'Item C', link: '/item-3' }
+        ]
+      }
+    ],
+    sidebar: [
+      {
+        text: 'Guide',
+        items: [
+          { text: 'Introduction', link: '/introduction' },
+          { text: 'Getting Started', link: '/getting-started' },
+        ],
+      }
+    ],
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+      { icon: 'twitter', link: '...' },
+      // You can also add custom icons by passing SVG as string:
+      {
+        icon: {
+          svg: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Dribbble</title><path d="M12...6.38z"/></svg>'
+        },
+        link: '...'
+      }
+    ]
+  }
+}
